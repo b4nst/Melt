b8ac976d944f9fddc418ab6ccba775fc55be22d0
Adrien Tisseraud <adrien@tartofrai.se>
2015-05-09 11:47:04 +0200

    Fix macros for Ableton test loading

--- QMelt/src/test/melttestcore.cpp
+++ QMelt/src/test/melttestcore.cpp
@@ -9,10 +9,14 @@
 
 
 #if defined (Q_OS_WIN)
-#define EXAMPLE_DIR "../Example/"
+#define M_EXAMPLE_DIR "../Example/"
 #elif defined (Q_OS_OSX)
-#define EXAMPLE_DIR "../../../../Example/"
+#define M_EXAMPLE_DIR "../../../../Example/"
 #endif
+
+
+#define M_PATH_ALS_KENYA QString(M_EXAMPLE_DIR) + QString("als-xml/sample_project_01.xml")
+#define M_PATH_ALS_ELATION QString(M_EXAMPLE_DIR) + QString("als-xml/sample_project_02.xml")
 
 
 M_NAMESPACE_TEST_BEGIN
@@ -20,10 +24,12 @@ M_NAMESPACE_TEST_BEGIN
 
 void MeltTestCore::testParser()
 {
-  QString filePath(EXAMPLE_DIR + QString("als-xml/sample_project_01.xml"));
+  QString filePath(M_PATH_ALS_KENYA);
   QSharedPointer<ableton::AlsAbleton> ableton_;
   io::AlsFilesystem::load(filePath, ableton_);
+  int i;
 
+  ++i;
 }
 
 
