f17fc46981c9960489f4d78e5ab1bcf48adf947f
Adrien Tisseraud <adrien@tartofrai.se>
2015-05-09 13:10:07 +0200

    Remove main window reference

--- QMelt/src/app/main.cpp
+++ QMelt/src/app/main.cpp
@@ -1,15 +1,10 @@
 // APP
 #include "src/app/meltapplication.h"
 
-// UI
-#include "src/ui/mainwindow.h"
-
 
 int main(int argc, char *argv[])
 {
     app::MeltApplication a(argc, argv);
-    ui::MainWindow w;
-    w.show();
 
     return a.exec();
 }
